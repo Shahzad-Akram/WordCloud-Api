b9aa2182b6b4bb442a4473ed539af553e3c7ce8f
Shahzad-Akram <shahzad.ftw45@gmail.com>
2020-02-12 23:03:34 +0500

    Fixed Chalk

--- Connection/dbConnect.js
+++ Connection/dbConnect.js
@@ -1,14 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import bold from 'chalk';
 
 dotenv.config();
-
-const { cyan, yellow, red } = bold;
-
-const connected = cyan;
-const error = yellow;
-const disconnected = red;
 
 // export this function and imported by server.js
 
@@ -21,17 +14,15 @@ const Connect = () => {
   mongoose.set('useCreateIndex', true);
 
   mongoose.connection.on('connected', () => {
-    console.log(
-      connected(`Mongoose default connection is open to ${process.env.DB_URI}`)
-    );
+    console.log(`Mongoose default connection is open to ${process.env.DB_URI}`);
   });
 
   mongoose.connection.on('error', err => {
-    console.log(error(`Mongoose default connection has occured ${err} error`));
+    console.log(`Mongoose default connection has occured ${err} error`);
   });
 
   mongoose.connection.on('disconnected', () => {
-    console.log(disconnected('Mongoose default connection is disconnected'));
+    console.log('Mongoose default connection is disconnected');
   });
 };
 
