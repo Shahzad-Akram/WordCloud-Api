50569406c10b2f1ac0e038047f6642a039249ef7
Shahzad-Akram <shahzad.ftw45@gmail.com>
2020-02-12 23:38:55 +0500

    Mongo Bullshit

--- Connection/dbConnect.js
+++ Connection/dbConnect.js
@@ -1,29 +1,16 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
-// export this function and imported by server.js
-
 const Connect = () => {
-  mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  mongoose.set('useFindAndModify', false);
-  mongoose.set('useCreateIndex', true);
-
-  mongoose.connection.on('connected', () => {
-    console.log(`Mongoose default connection is open to ${process.env.DB_URI}`);
-  });
-
-  mongoose.connection.on('error', err => {
-    console.log(`Mongoose default connection has occured ${err} error`);
-  });
-
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose default connection is disconnected');
-  });
+  mongoose
+    .connect(
+      'DB_URI=mongodb+srv://digisol:digisol@cluster0-oudlk.mongodb.net/wordcloud-api?retryWrites=true&w=majority',
+      () => {},
+      { useNewUrlParser: true }
+    )
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 export default Connect;
