a0457353186bd88b14bf13bed6d804b5e174767c
OmerFarooq <comingbacktolyf@gmail.com>
2020-02-13 11:47:31 +0500

    Fuck this App

--- Connection/dbConnect.js
+++ Connection/dbConnect.js
@@ -1,16 +1,38 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import bold from 'chalk';
+
+dotenv.config();
+
+const { cyan, yellow, red } = bold;
+
+const connected = cyan;
+const error = yellow;
+const disconnected = red;
+
+// export this function and imported by server.js
 
 const Connect = () => {
-  mongoose
-    .connect(
-      'DB_URI=mongodb+srv://digisol:digisol@cluster0-oudlk.mongodb.net/wordcloud-api?retryWrites=true&w=readPreference=secondary&ssl=true',
-      () => {},
-      { useNewUrlParser: true }
-    )
-    .catch(err => {
-      console.log(err);
-    });
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  mongoose.set('useFindAndModify', false);
+  mongoose.set('useCreateIndex', true);
+
+  mongoose.connection.on('connected', () => {
+    console.log(
+      connected(`Mongoose default connection is open to ${process.env.DB_URI}`)
+    );
+  });
+
+  mongoose.connection.on('error', err => {
+    console.log(error(`Mongoose default connection has occured ${err} error`));
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log(disconnected('Mongoose default connection is disconnected'));
+  });
 };
 
 export default Connect;
